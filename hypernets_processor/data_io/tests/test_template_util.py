a253374230331a406ea56a787215732ed890bcce
seh2 <sam.hunt@npl.co.uk>
2020-02-23 11:22:41 +0000

    add tests for adding metadata methods

---
+++ hypernets_processor/data_io/tests/test_template_util.py
@@ -0,0 +1,59 @@
+"""
+Tests for TemplateUtil class
+"""
+
+import unittest
+import xarray
+from hypernets_processor.data_io.template_util import TemplateUtil
+
+
+'''___Authorship___'''
+__author__ = "Sam Hunt"
+__created__ = "21/2/2020"
+__version__ = "0.0"
+__maintainer__ = "Sam Hunt"
+__email__ = "sam.hunt@npl.co.uk"
+__status__ = "Development"
+
+
+class TestTemplateUtil(unittest.TestCase):
+    def test_add_common_metadata(self):
+        dataset = xarray.Dataset()
+
+        TemplateUtil.add_common_metadata(dataset)
+
+        self.assertEqual("eng", dataset.attrs["metadata_language"])
+
+    def test_add_l1_metadata(self):
+        dataset = xarray.Dataset()
+
+        TemplateUtil.add_l1_metadata(dataset)
+
+        self.assertEqual("HYPERNETS network dataset of downwelling irradiance and upwelling and downwelling radiance",
+                         dataset.attrs["resource_title"])
+
+    def test_add_l2_metadata(self):
+        dataset = xarray.Dataset()
+
+        TemplateUtil.add_l2_metadata(dataset)
+
+        self.assertEqual("HYPERNETS network dataset of spectral surface reflectance",
+                         dataset.attrs["resource_title"])
+
+    def test_add_land_network_metadata(self):
+        dataset = xarray.Dataset()
+
+        TemplateUtil.add_land_network_metadata(dataset)
+
+        self.assertEqual("Hunt Sam", dataset.attrs["creator_name"])
+
+    def test_add_water_network_metadata(self):
+        dataset = xarray.Dataset()
+
+        TemplateUtil.add_water_network_metadata(dataset)
+
+        self.assertEqual("Goyens Clémence", dataset.attrs["creator_name"])
+
+
+if __name__ == '__main__':
+    unittest.main()
