8ae766dca205c9eee13eca9fb8e18ae200142d05
seh2 <sam.hunt@npl.co.uk>
2020-03-23 12:13:53 +0000

    add place holder for hypernets reader functionality

---
+++ hypernets_processor/data_io/hypernets_reader.py
@@ -0,0 +1,29 @@
+"""
+Module with functions and classes to read Hypernets data
+"""
+
+import xarray as xr
+
+
+'''___Authorship___'''
+__author__ = "Sam Hunt"
+__created__ = "12/2/2020"
+__version__ = "0.0"
+__maintainer__ = "Sam Hunt"
+__email__ = "sam.hunt@npl.co.uk"
+__status__ = "Development"
+
+
+def read_hypernets_data(filename):
+
+    dataset = None
+
+    return dataset
+
+
+class HypernetsReader:
+    pass
+
+
+if __name__ == '__main__':
+    pass
