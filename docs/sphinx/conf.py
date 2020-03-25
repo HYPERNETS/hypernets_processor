bc57cbfcf63be29876c60f8a2d21ce04850abb79
samehunt <sam.hunt@npl.co.uk>
2020-03-25 14:14:22 +0000

    Move images location

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -172,13 +172,13 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = "content/images/hypernets-logo.png"
+html_logo = "../images/hypernets-banner.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-html_favicon = "content/images/hypernets-logo.ico"
+html_favicon = "../images/hypernets-logo.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
