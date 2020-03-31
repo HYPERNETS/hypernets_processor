2e8e8db57cc75aebbe8d9c1c18f329f5ab660339
samehunt <sam.hunt@npl.co.uk>
2020-03-31 10:15:13 +0100

    add paths modules for paths operations, including function to get path relative to given directory

---
+++ hypernets_processor/utils/paths.py
@@ -0,0 +1,42 @@
+"""
+Module of helper functions for file path operations
+"""
+
+
+from hypernets_processor.version import __version__
+import os
+
+
+'''___Authorship___'''
+__author__ = "Sam Hunt"
+__created__ = "31/3/2020"
+__version__ = __version__
+__maintainer__ = "Sam Hunt"
+__email__ = "sam.hunt@npl.co.uk"
+__status__ = "Development"
+
+
+def relative_path(path, directory):
+    """
+    Return path relative to given directory
+
+    :type path: str
+    :param path: path of file relative to directory
+
+    :type directory: str
+    :param directory: path of directory
+
+    :return: full file path
+    :rtype: str
+    """
+
+    cwd = os.getcwd()
+    os.chdir(directory)
+    full_path = os.path.abspath(path)
+    os.chdir(cwd)
+
+    return full_path
+
+
+if __name__ == "__main__":
+    pass
---
+++ hypernets_processor/utils/tests/test_paths.py
@@ -0,0 +1,53 @@
+"""
+Tests for paths module
+"""
+
+import unittest
+from hypernets_processor.version import __version__
+from hypernets_processor.utils.paths import relative_path
+import os
+
+
+'''___Authorship___'''
+__author__ = "Sam Hunt"
+__created__ = "21/2/2020"
+__version__ = __version__
+__maintainer__ = "Sam Hunt"
+__email__ = "sam.hunt@npl.co.uk"
+__status__ = "Development"
+
+
+class TestPaths(unittest.TestCase):
+    def test_relative_path_in_directory(self):
+
+        # Use data directory for tests
+        data_directory = "../../../data/tests/cli"
+        fname = "jobs1.config"
+
+        full_path = relative_path(fname, data_directory)
+
+        self.assertEqual(full_path, os.path.abspath("../../../data/tests/cli/jobs1.config"))
+
+    def test_relative_path_in_subdirectory(self):
+
+        # Use data directory for tests
+        data_directory = "../../../data"
+        fname = "tests/cli/jobs1.config"
+
+        full_path = relative_path(fname, data_directory)
+
+        self.assertEqual(full_path, os.path.abspath("../../../data/tests/cli/jobs1.config"))
+
+    def test_relative_path_in_relative_directory(self):
+
+        # Use data directory for tests
+        data_directory = "../../../data/tests/reader"
+        fname = "../cli/jobs1.config"
+
+        full_path = relative_path(fname, data_directory)
+
+        self.assertEqual(full_path, os.path.abspath("../../../data/tests/cli/jobs1.config"))
+
+
+if __name__ == "__main__":
+    unittest.main()
