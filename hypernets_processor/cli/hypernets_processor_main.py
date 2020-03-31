60d186c2001a614ce6507ea342917cb93f85217d
samehunt <sam.hunt@npl.co.uk>
2020-03-31 17:11:14 +0100

    update hypernets_process_cli files to reflect changes to config file reading

--- hypernets_processor/cli/hypernets_processor_main.py
+++ hypernets_processor/cli/hypernets_processor_main.py
@@ -4,6 +4,8 @@ Main function for hypernets_processor_cli to run
 
 from hypernets_processor.version import __version__
 from hypernets_processor.hypernets_processor import HypernetsProcessor
+from hypernets_processor.cli.common import configure_logging
+
 
 '''___Authorship___'''
 __author__ = "Sam Hunt"
@@ -14,14 +16,28 @@ __email__ = "sam.hunt@npl.co.uk"
 __status__ = "Development"
 
 
-def main(logger=None):
+def main(processor_config, job_config):
     """
     Main function to run hypernets_processor
 
-    :type logger: logging.logger
-    :param logger: logger
+    :type processor_config: dict
+    :param processor_config: processor configuration information, with entries:
+
+    * ...
+
+    :type job_config: dict
+    :param job_config: job configuration information, with entries:
+
+    * log_path (str) - Path to write log to, default None (means log goes to stdout)
+    * verbose (bool) - Switch for verbose output, default False
+    * quiet (bool) - Switch for quiet output, default False
+
     """
 
+    # Configure logging
+    logger = configure_logging(fname=job_config["log_path"], verbose=job_config["verbose"], quiet=job_config["quiet"])
+
+    # Run processor
     hp = HypernetsProcessor(logger=logger)
     hp.run()
 
