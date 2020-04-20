a9f4004be9dc0ba48a01fee1e5ae1de6ca712dfb
Pieter De Vis <pieter.de.vis@npl.co.uk>
2020-04-20 17:56:42 +0100

    calibration and protocols

---
+++ hypernets_processor/calibration/calibrate.py
@@ -0,0 +1,141 @@
+"""
+Calibration class
+"""
+
+from hypernets_processor.version import __version__
+from hypernets_processor.data_io.hypernets_writer import HypernetsWriter
+from hypernets_processor.measurement_functions.measurement_function_factory import MeasurementFunctionFactory
+import punpy
+
+'''___Authorship___'''
+__author__ = "Pieter De Vis"
+__created__ = "12/04/2020"
+__version__ = __version__
+__maintainer__ = "Pieter De Vis"
+__email__ = "Pieter.De.Vis@npl.co.uk"
+__status__ = "Development"
+
+class Calibrate:
+    def __init__(self,MCsteps=10000):
+        self._measurement_function_factory = MeasurementFunctionFactory()
+        self.prop= punpy.MCPropagation(MCsteps)
+
+    def calibrate(self,dataset_l0,calibration_data,measurement_function='StandardMeasurementFunction'):
+        dataset_l0 = self.preprocess_l0(dataset_l0)
+        calibrate_function = self._measurement_function_factory.get_measurement_function(measurement_function)
+        input_vars=calibrate_function.get_argument_names()
+        input_qty=self.find_input(input_vars,dataset_l0,calibration_data)
+        u_random_input_qty=self.find_u_random_input(input_vars,dataset_l0,calibration_data)
+        u_systematic_input_qty=self.find_u_systematic_input(input_vars,dataset_l0,calibration_data
+        dataset_l1 = self.l1_from_l0_dataset(dataset_l0)
+        dataset_l1 = self.process_measurement_function(dataset_l1,calibrate_function.function(),input_qty,u_random_input_qty,u_systematic_input_qty)
+        return dataset_l1
+
+    def find_input(self,variables,dataset,ancillary_dataset):
+        """
+        returns a list of the data for a given list of input variables
+
+        :param variables:
+        :type variables:
+        :param dataset:
+        :type dataset:
+        :return:
+        :rtype:
+        """
+        inputs = []
+        for var in range(variables):
+            try:
+                inputs.append(dataset[var])
+            except:
+                inputs.append(ancillary_dataset[var])
+        return inputs
+
+    def find_u_random_input(self,variables,dataset,ancillary_dataset):
+        """
+        returns a list of the random uncertainties on the data for a given list of input variables
+
+        :param variables:
+        :type variables:
+        :param dataset:
+        :type dataset:
+        :return:
+        :rtype:
+        """
+        inputs = []
+        for var in range(variables):
+            try:
+                inputs.append(dataset["u_random_"+var])
+            except:
+                inputs.append(ancillary_dataset["u_random_"+var])
+        return inputs
+
+    def find_u_systematic_input(self,variables,dataset,ancillary_dataset):
+        """
+        returns a list of the systematic uncertainties on the data for a given list of input variables
+
+        :param variables:
+        :type variables:
+        :param dataset:
+        :type dataset:
+        :return:
+        :rtype:
+        """
+        inputs = []
+        for var in range(variables):
+            try:
+                inputs.append(dataset["u_systematic_"+var])
+            except:
+                inputs.append(ancillary_dataset["u_systematic_"+var])
+
+        return inputs
+
+
+    def preprocess_l0(self,dataset_l0):
+        """
+        Identifies and removes faulty measurements (e.g. due to cloud cover).
+
+        :param dataset_l0:
+        :type dataset_l0:
+        :return:
+        :rtype:
+        """
+        return dataset_l0
+    
+    def l1_from_l0_dataset(self,datasetl0):
+        """
+        Makes a L1 template of the data, and propagates the appropriate keywords from L0.
+
+        :param datasetl0:
+        :type datasetl0:
+        :return:
+        :rtype:
+        """
+        hw = HypernetsWriter()
+        dataset = hw.create_template_dataset_l1_rad()
+
+        return dataset
+
+    def process_measurement_function(self,dataset_l1,measurement_function,input_quantities,u_random_input_quantities,u_systematic_input_quantities):
+        measurand = measurement_function(*input_quantities)
+        u_random_measurand = self.prop.propagate_random(measurement_function,input_quantities,u_random_input_quantities)
+        u_systematic_measurand = self.prop.propagate_systematic(measurement_function,input_quantities,u_systematic_input_quantities)
+        u_tot_measurand, cov_measurand = self.prop.propagate_both(measurement_function,input_quantities,u_random_input_quantities,u_systematic_input_quantities)
+
+        dataset_l1["radiance"]=measurand
+        dataset_l1["u_random_radiance"]=u_random_measurand
+        dataset_l1["u_systematic_radiance"]=u_systematic_measurand
+        dataset_l1["cov_radiance"]=cov_measurand
+
+        return dataset_l1
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hypernets_processor/measurement_functions/standard_measurement_function.py
@@ -0,0 +1,21 @@
+
+
+
+
+class StandardMeasurementFunction:
+
+    def function(self,counts,gains,dark_signal,temp):
+        '''
+        This function implements the measurement function.
+        Each of the arguments can be either a scalar or a vector (1D-array).
+        '''
+
+        return gains*(counts-dark_signal)+temp**0.3
+
+    @staticmethod
+    def get_name():
+        return "StandardMeasurementFunction"
+
+    def get_argument_names(self):
+        return ["counts","gains","dark_signal","temp"]
+
