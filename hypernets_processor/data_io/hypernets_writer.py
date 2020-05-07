cccb3fd118cb01bcd8a633f00058a6654e9dac33
shunt16 <sam.hunt@npl.co.uk>
2020-05-07 10:35:15 +0100

    separate filename generation into separate class

--- hypernets_processor/data_io/hypernets_writer.py
+++ hypernets_processor/data_io/hypernets_writer.py
@@ -15,10 +15,6 @@ __email__ = "sam.hunt@npl.co.uk"
 __status__ = "Development"
 
 
-TIME_FMT_L12A = "%Y%m%d%H%M"
-TIME_FMT_L2B = "%Y%m%d"
-
-
 class HypernetsWriter:
     """
     Class to write Hypernets output files
@@ -71,106 +67,6 @@ class HypernetsWriter:
 
         else:
             raise NameError("Invalid fmt: "+fmt)
-
-    @staticmethod
-    def create_file_name_l1a_rad(network, site, time, version):
-        """
-        Return a valid file name for Hypernets Level 1a radiance file
-
-        :type network: str
-        :param network: abbreviated network name
-
-        :type site: str
-        :param site: abbreviated site name
-
-        :type time: datetime.datetime
-        :param time: acquisition time
-
-        :type version: str
-        :param version: processing version
-
-        :return: Level 1 filename
-        :rtype: str
-        """
-
-        time_string = time.strftime(TIME_FMT_L12A)
-        return HypernetsWriter._create_file_name(network, site, "RAD", time_string, version)
-
-    @staticmethod
-    def create_file_name_l1a_irr(network, site, time, version):
-        """
-        Return a valid file name for Hypernets Level 1a irradiance file
-
-        :type network: str
-        :param network: abbreviated network name
-
-        :type site: str
-        :param site: abbreviated site name
-
-        :type time: datetime.datetime
-        :param time: acquisition time
-
-        :type version: str
-        :param version: processing version
-
-        :return: Level 1 filename
-        :rtype: str
-        """
-
-        time_string = time.strftime(TIME_FMT_L12A)
-        return HypernetsWriter._create_file_name(network, site, "IRR", time_string, version)
-
-    @staticmethod
-    def create_file_name_l2a(network, site, time, version):
-        """
-        Return a valid file name for Hypernets Level 2a file
-
-        :type network: str
-        :param network: abbreviated network name
-
-        :type site: str
-        :param site: abbreviated site name
-
-        :type time: datetime.datetime
-        :param time: acquisition time
-
-        :type version: str
-        :param version: processing version
-
-        :return: Level 2a filename
-        :rtype: str
-        """
-
-        time_string = time.strftime(TIME_FMT_L12A)
-        return HypernetsWriter._create_file_name(network, site, "REF", time_string, version)
-
-    @staticmethod
-    def create_file_name_l2b(network, site, time, version):
-        """
-        Return a valid file name for Hypernets Level 2b file
-
-        :type network: str
-        :param network: abbreviated network name
-
-        :type site: str
-        :param site: abbreviated site name
-
-        :type time: datetime.datetime
-        :param time: acquisition time
-
-        :type version: str
-        :param version: processing version
-
-        :return: Level 2b filename
-        :rtype: str
-        """
-
-        time_string = time.strftime(TIME_FMT_L2B)
-        return HypernetsWriter._create_file_name(network, site, "REFD", time_string, version)
-
-    @staticmethod
-    def _create_file_name(network, site, ptype, time_string, version):
-        return "_".join(["HYPERNETS", network.upper(), site.upper(), ptype, time_string, "v"+version]) + ".nc"
 
 
 if __name__ == '__main__':
---
+++ hypernets_processor/data_io/tests/test_filename_util.py
@@ -0,0 +1,44 @@
+"""
+Tests for FilenameUtil class
+"""
+
+import unittest
+import datetime
+from hypernets_processor.data_io.filename_util import FilenameUtil
+from hypernets_processor.version import __version__
+
+
+'''___Authorship___'''
+__author__ = "Sam Hunt"
+__created__ = "7/5/2020"
+__version__ = __version__
+__maintainer__ = "Sam Hunt"
+__email__ = "sam.hunt@npl.co.uk"
+__status__ = "Development"
+
+
+class TestFilenameUtil(unittest.TestCase):
+
+    def test_create_file_name_l1a_irr(self):
+
+        fname = FilenameUtil.create_file_name_l1a_irr("L", "GBNA", datetime.datetime(2018, 4, 3, 11, 00, 00), "0.00")
+        self.assertEqual("HYPERNETS_L_GBNA_IRR_201804031100_v0.00.nc", fname)
+
+    def test_create_file_name_l1a_rad(self):
+
+        fname = FilenameUtil.create_file_name_l1a_rad("L", "GBNA", datetime.datetime(2018, 4, 3, 11, 00, 00), "0.00")
+        self.assertEqual("HYPERNETS_L_GBNA_RAD_201804031100_v0.00.nc", fname)
+
+    def test_create_file_name_l2a(self):
+
+        fname = FilenameUtil.create_file_name_l2a("L", "GBNA", datetime.datetime(2018, 4, 3, 11, 00, 00), "0.00")
+        self.assertEqual("HYPERNETS_L_GBNA_REF_201804031100_v0.00.nc", fname)
+
+    def test_create_file_name_l2b(self):
+
+        fname = FilenameUtil.create_file_name_l2b("L", "GBNA", datetime.datetime(2018, 4, 3, 11, 00, 00), "0.00")
+        self.assertEqual("HYPERNETS_L_GBNA_REFD_20180403_v0.00.nc", fname)
+
+
+if __name__ == '__main__':
+    unittest.main()
