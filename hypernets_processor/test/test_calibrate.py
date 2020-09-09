bce78527f1f2a94c58e7a3139ad444049efa63e6
Pieter De Vis <pieter.de.vis@npl.co.uk>
2020-09-09 18:22:24 +0100

    test structure

--- hypernets_processor/test/test_calibrate.py
+++ hypernets_processor/test/test_calibrate.py
@@ -19,39 +19,50 @@ __status__ = "Development"
 
 class TestCalibrate(unittest.TestCase):
 
-    def test_calibrate_l1a(self,measurandstring,dataset_l0,dataset_l0_bla,calibration_data,
-                      measurement_function='StandardMeasurementFunction'):
+    def test_here(self):
+        self.assertEqual(1,1)
 
-
-
-    def test_average_l1b(self,measurandstring,dataset_l1a):
-
-    def test_calc_mean_masked(self,dataset,var,rand_unc=False,corr=False):
-
-    def test_find_nearest_black(self,dataset,acq_time,int_time):
-
-    def test_find_input(self,variables,dataset,datasetbla,ancillary_dataset):
-
-    def test_find_u_random_input(self,variables,dataset,datasetbla,ancillary_dataset,masked_avg=False):
-
-
-    def test_find_u_systematic_input(self,variables,dataset,datasetbla,ancillary_dataset,masked_avg=False):
-
-
-    def test_preprocess_l0(self,datasetl0,datasetl1a):
-
-    def test_clip_and_mask(self,dataset,k_unc=2):
-
-
-    def test_sigma_clip(self,values,tolerance=0.001,median=True,sigma_thresh=3.0):
-
-
-    def test_l1a_template_from_l0_dataset(self,measurandstring,datasetl0):
-
-
-    def test_process_measurement_function(self,measurandstring,dataset,measurement_function,input_quantities,
-                                     u_random_input_quantities,u_systematic_input_quantities):
-
+#
+    # def test_calibrate_l1a(self,measurandstring,dataset_l0,dataset_l0_bla,calibration_data,
+    #                   measurement_function='StandardMeasurementFunction'):
+    #     self.assertEqual(1,1)
+    #
+    #
+    # def test_average_l1b(self,measurandstring,dataset_l1a):
+    #     self.assertEqual(1,1)
+    #
+    # def test_calc_mean_masked(self,dataset,var,rand_unc=False,corr=False):
+    #     self.assertEqual(1,1)
+    #
+    # def test_find_nearest_black(self,dataset,acq_time,int_time):
+    #     self.assertEqual(1,1)
+    #
+    # def test_find_input(self,variables,dataset,datasetbla,ancillary_dataset):
+    #     self.assertEqual(1,1)
+    #
+    # def test_find_u_random_input(self,variables,dataset,datasetbla,ancillary_dataset,masked_avg=False):
+    #     self.assertEqual(1,1)
+    #
+    # def test_find_u_systematic_input(self,variables,dataset,datasetbla,ancillary_dataset,masked_avg=False):
+    #     self.assertEqual(1,1)
+    #
+    # def test_preprocess_l0(self,datasetl0,datasetl1a):
+    #     self.assertEqual(1,1)
+    #
+    # def test_clip_and_mask(self,dataset,k_unc=2):
+    #     self.assertEqual(1,1)
+    #
+    # def test_sigma_clip(self,values,tolerance=0.001,median=True,sigma_thresh=3.0):
+    #     self.assertEqual(1,1)
+    #
+    # def test_l1a_template_from_l0_dataset(self,measurandstring,datasetl0):
+    #     self.assertEqual(1,1)
+    #
+    #
+    # def test_process_measurement_function(self,measurandstring,dataset,measurement_function,input_quantities,
+    #                                  u_random_input_quantities,u_systematic_input_quantities):
+    #     self.assertEqual(1,1)
+    #
 
 if __name__ == '__main__':
-    unittest.main()
+    pass
