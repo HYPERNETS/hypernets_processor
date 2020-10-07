5784b823fe536584ffdc3e9db33afa86c5f1ca42
Pieter De Vis <pieter.de.vis@npl.co.uk>
2020-10-07 16:39:05 +0100

    fix clemence's updates

--- hypernets_processor/surface_reflectance/surface_reflectance.py
+++ hypernets_processor/surface_reflectance/surface_reflectance.py
@@ -6,7 +6,7 @@ from hypernets_processor.version import __version__
 from hypernets_processor.data_io.hypernets_ds_builder import HypernetsDSBuilder
 from hypernets_processor.data_io.hypernets_writer import HypernetsWriter
 from hypernets_processor.surface_reflectance.measurement_functions.protocol_factory import ProtocolFactory
-from hypernets_processor.calibration import Calibrate
+from hypernets_processor.calibration.calibrate import Calibrate
 
 import punpy
 import numpy as np
@@ -26,7 +26,7 @@ class SurfaceReflectance:
         self.prop= punpy.MCPropagation(MCsteps,parallel_cores=parallel_cores)
         self.hdsb = HypernetsDSBuilder(context=context)
         self.writer=HypernetsWriter(context)
-        self.calibrate=Calibration(context)
+        self.calibrate=Calibrate(context)
         self.context = context
 
     def process(self,dataset_l1c):
@@ -36,38 +36,39 @@ class SurfaceReflectance:
         input_qty = self.find_input(input_vars,dataset_l1c)
         u_random_input_qty = self.find_u_random_input(input_vars,dataset_l1c)
         u_systematic_input_qty = self.find_u_systematic_input(input_vars,dataset_l1c)
-        dataset_l1d = self.l1d_from_l1c_dataset(dataset_l1c)
-        dataset_l2a = self.l2_from_l1c_dataset(dataset_l1c)
 
         if self.context.get_config_value("network")=="W":
+            dataset_l1d = self.l1d_from_l1c_dataset(dataset_l1c)
+            dataset_l2a = self.l2_from_l1d_dataset(dataset_l1d)
 
             dataset_l1d = self.process_measurement_function(["nlw","rhow_nosc","rhow"],
                 dataset_l1d,l1tol2_function.function,input_qty,u_random_input_qty,
                 u_systematic_input_qty)
-
-            dataset_l2a[measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d, measurandstring)
-            dataset_l2a["u_random_" + measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d,
-                                                                                      "u_random_" + measurandstring,
-                                                                                      rand_unc=True)
-            dataset_l2a["u_systematic_" + measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d,
-                                                                                          "u_systematic_" + measurandstring)
-            dataset_l2a["corr_random_" + measurandstring].values = np.eye(
-                len(dataset_l2a["u_systematic_" + measurandstring].values))
-            dataset_l2a["corr_systematic_" + measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d,
-                                                                                             "corr_systematic_" + measurandstring,
-                                                                                             corr=True)
-            self.writer.write(dataset_l2,overwrite=True)
+            for measurandstring in ["nlw","rhow_nosc","rhow"]:
+                dataset_l2a[measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d, measurandstring)
+                dataset_l2a["u_random_" + measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d,
+                                                                                          "u_random_" + measurandstring,
+                                                                                          rand_unc=True)
+                dataset_l2a["u_systematic_" + measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d,
+                                                                                              "u_systematic_" + measurandstring)
+                dataset_l2a["corr_random_" + measurandstring].values = np.eye(
+                    len(dataset_l2a["u_systematic_" + measurandstring].values))
+                dataset_l2a["corr_systematic_" + measurandstring].values = self.calibrate.calc_mean_masked(dataset_l1d,
+                                                                                                 "corr_systematic_" + measurandstring,
+                                                                                                 corr=True)
+            self.writer.write(dataset_l2a,overwrite=True)
             self.writer.write(dataset_l1d,overwrite=True)
-            return dataset_l2, dataset_l1d
+            return dataset_l2a, dataset_l1d
 
         elif self.context.get_config_value("network")=="L":
-            dataset_l2 = self.process_measurement_function(["reflectance"],dataset_l2,
+            dataset_l2a = self.l2_from_l1c_dataset(dataset_l1c)
+            dataset_l2a = self.process_measurement_function(["reflectance"],dataset_l2a,
                                                            l1tol2_function.function,
                                                            input_qty,u_random_input_qty,
                                                            u_systematic_input_qty)
 
-            self.writer.write(dataset_l2,overwrite=True)
-            return dataset_l2
+            self.writer.write(dataset_l2a,overwrite=True)
+            return dataset_l2a
 
     def find_input(self,variables,dataset):
         """
@@ -145,13 +146,14 @@ class SurfaceReflectance:
         :rtype:
         """
         if self.context.get_config_value("network") == "L":
-            print("No L1D level for land)
-        elif self.context.get_config_value("network") == "W":
-            l2a_dim_sizes_dict = {"wavelength": len(datasetl1c["wavelength"]),
-                                  "scan": len(datasetl1c['scan'])}
-            l2a = self.hdsb.create_ds_template(l2a_dim_sizes_dict, "W_L1D")
+            print("No L1D level for land")
 
-        return l1d
+        elif self.context.get_config_value("network") == "W":
+            l1d_dim_sizes_dict = {"wavelength": len(datasetl1c["wavelength"]),
+                                  "scan": len(datasetl1c["scan"])}
+            dataset_l1d = self.hdsb.create_ds_template(l1d_dim_sizes_dict, "W_L1D", propagate_ds=datasetl1c)
+
+        return dataset_l1d
 
     def l2_from_l1c_dataset(self,datasetl1c):
         """
@@ -164,15 +166,32 @@ class SurfaceReflectance:
         """
         if self.context.get_config_value("network") == "L":
             l2a_dim_sizes_dict = {"wavelength":len(datasetl1c["wavelength"]),
-                                  "series":len(datasetl1c['series'])}
-            l2a = self.hdsb.create_ds_template(l2a_dim_sizes_dict, "L_L2A")
+                                  "series":len(datasetl1c['series_id'])}
+            dataset_l2a = self.hdsb.create_ds_template(l2a_dim_sizes_dict, "L_L2A", propagate_ds=datasetl1c)
 
-        elif self.context.get_config_value("network") == "W":
-            l2a_dim_sizes_dict = {"wavelength": len(datasetl1c["wavelength"]),
-                                  "series": len(datasetl1c['series'])}
-            l2a = self.hdsb.create_ds_template(l2a_dim_sizes_dict, "W_L2A")
+        return dataset_l2a
 
-        return l2a
+    def l2_from_l1d_dataset(self,datasetl1d):
+
+        if self.context.get_config_value("network") == "W":
+            l2a_dim_sizes_dict = {"wavelength": len(datasetl1d["wavelength"]),
+                                  "series": len(np.unique(datasetl1d['series_id']))}
+            dataset_l2a = self.hdsb.create_ds_template(l2a_dim_sizes_dict, "W_L2A", propagate_ds=datasetl1d)
+
+            series_id = np.unique(datasetl1d['series_id'])
+            dataset_l2a["series_id"].values = series_id
+
+            for variablestring in ["acquisition_time","viewing_azimuth_angle",
+                                   "viewing_zenith_angle","solar_azimuth_angle",
+                                   "solar_zenith_angle"]:
+                temp_arr = np.empty(len(series_id))
+                for i in range(len(series_id)):
+                    ids = np.where((datasetl1d['series_id'] == series_id[i]) & (
+                            datasetl1d['quality_flag'] == 1))
+                    temp_arr[i] = np.mean(datasetl1d[variablestring].values[ids])
+                dataset_l2a[variablestring].values = temp_arr
+
+        return dataset_l2a
 
     def process_measurement_function(self,measurandstrings,dataset,measurement_function,input_quantities,u_random_input_quantities,
                                      u_systematic_input_quantities):
