e60bf2683ef18fc2b2154a41757d050d72f18e6c
shunt16 <sam.hunt@npl.co.uk>
2020-10-15 14:57:03 +0100

    add command to run processor setup on install

--- setup.py
+++ setup.py
@@ -1,39 +1,122 @@
 from io import open
 import os
-
+import sys
 from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 # Get package __version__.
 # Same effect as "from s import __version__",
 # but avoids importing the module which may not be installed yet:
 __version__ = None
 here = os.path.abspath(os.path.dirname(__file__))
-with open('hypernets_processor/version.py') as f:
+with open("hypernets_processor/version.py") as f:
     exec(f.read())
 
 # Get the long description from the README file
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='hypernets_processor',
-      version=__version__,
-      description='Software for processing Hypernets field data',
-      authors=['Sam Hunt', 'Clémence Goyens', 'Pieter De Vis'],
-      long_description=long_description,
-      author_email='sam.hunt@npl.co.uk',
-      url='http://hypernets.eu',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      include_package_data=True,
-      package_data={
-          'hypernets_processor': [os.path.join('etc', 'processor.config'),
-                                  os.path.join('etc', 'scheduler.config'),
-                                  os.path.join('etc', 'jobs.txt')],
 
-      },
-      install_requires=['numpy', 'netCDF4', 'xarray', 'schedule', 'matplotlib', 'punpy', 'dataset', 'pysolar', 'sqlalchemy_utils'],
-      entry_points={
-          'console_scripts': [
-                'hypernets_processor =  hypernets_processor.cli.hypernets_processor_cli:cli'
-          ],
-                    }
-      )
+def pre_install_processor_setup():
+    pass
+
+
+def post_install_processor_setup():
+    sys.argv = sys.argv[:1]
+    from hypernets_processor.cli.setup_processor_cli import cli
+
+    print("\nSet up processor\n----------------\n")
+    cli()
+
+    return None
+
+
+class SetupDevelopCommand(develop):
+    """
+    Custom installation for development mode.
+    """
+
+    user_options = develop.user_options + [
+        ("setup-processor", None, "Option to run additional processor setup."),
+    ]
+
+    def initialize_options(self):
+        develop.initialize_options(self)
+        self.setup_processor = 0
+
+    def run(self):
+
+        if self.setup_processor:
+            pre_install_processor_setup()
+
+        develop.run(self)
+
+        if self.setup_processor:
+            post_install_processor_setup()
+
+
+class SetupInstallCommand(install):
+    """
+    Custom installation for installation mode
+    """
+
+    user_options = install.user_options + [
+        ("setup-processor", None, "Option to run additional processor setup."),
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.setup_processor = 0
+
+    def run(self):
+
+        if self.setup_processor:
+            pre_install_processor_setup()
+
+        install.run(self)
+
+        if self.setup_processor:
+
+            post_install_processor_setup()
+
+
+setup(
+    name="hypernets_processor",
+    version=__version__,
+    description="Software for processing Hypernets field data",
+    authors=["Sam Hunt", "Clemence Goyens"],
+    long_description=long_description,
+    author_email="sam.hunt@npl.co.uk",
+    url="http://hypernets.eu",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    include_package_data=True,
+    cmdclass={
+        "develop": SetupDevelopCommand,
+        "install": SetupInstallCommand,
+    },
+    package_data={
+        "hypernets_processor": [
+            os.path.join("etc", "processor.config"),
+            os.path.join("etc", "scheduler.config"),
+            os.path.join("etc", "jobs.txt"),
+        ],
+    },
+    install_requires=[
+        "numpy",
+        "netCDF4",
+        "xarray",
+        "schedule",
+        "matplotlib",
+        "pysolar",
+        "dataset",
+        "sqlalchemy-utils",
+        "punpy",
+        "psycopg2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hypernets_processor =  hypernets_processor.cli.hypernets_processor_cli:cli"
+        ],
+    },
+)
