9cd67db262549735bd9b49b55f4063665b30f917
shunt16 <sam.hunt@npl.co.uk>
2020-10-20 10:13:02 +0100

    initialise job init

---
+++ hypernets_processor/cli/init_job_main.py
@@ -0,0 +1,29 @@
+"""
+Main function for job init
+"""
+
+from hypernets_processor.version import __version__
+
+
+'''___Authorship___'''
+__author__ = "Sam Hunt"
+__created__ = "20/10/2020"
+__version__ = __version__
+__maintainer__ = "Sam Hunt"
+__email__ = "sam.hunt@npl.co.uk"
+__status__ = "Development"
+
+
+def main(settings):
+    """
+    Main function for job init
+
+    :type settings: dict
+    :param settings: user defined configuration values
+    """
+
+    return None
+
+
+if __name__ == "__main__":
+    pass
