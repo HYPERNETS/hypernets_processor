65dcaf0e6ebf3ab20ae69c9bcdf166e5c5b35b94
pdevis <pieter.devis1@gmail.com>
2020-12-01 10:14:16 +0000

    Include plots (#99)
    * final edits for v 0.1
    
    * fixing uncertainties and correlation matrices
    
    * include covariance plots
    
    * fix repeated measurements with using corr
    
    * fix repeated measurements with using corr
    Using updated calibration files
    Interpolate wavelengths of irradiance to radiance wavelengths
    
    * test
    
    * interpolate common for both networks
    
    * interpolate common for both networks
    
    * interpolate common for both networks
    
    Co-authored-by: Pieter De Vis <pieter.de.vis@npl.co.uk>

---
+++ hypernets_processor/interpolation/measurement_functions/interpolate_wav_linear.py
@@ -0,0 +1,24 @@
+
+
+import scipy.interpolate
+import numpy as np
+
+class InterpolationWavLinear:
+    def function(self,rad_wavs,irr_wavs,irr):
+        '''
+        This function implements the measurement function.
+        Each of the arguments can be either a scalar or a vector (1D-array).
+        '''
+        irradiance_intfunc = scipy.interpolate.interp1d(irr_wavs,irr,axis=0,
+                                                        fill_value="extrapolate")
+        out = irradiance_intfunc(rad_wavs)
+        return out
+
+
+    @staticmethod
+    def get_name():
+        return "InterpolationWavLinear"
+
+    def get_argument_names(self):
+        return ["radiance_wavelengths","irradiance_wavelengths","irradiances"]
+
