8e51d972c09b8528b85a71bcb5ce3ba870e9f932
cgoyens <cgoyens@naturalsciences.be>
2022-12-02 17:21:18 +0100

    add l0rad and l0irr masked for proc l1b in water proc

--- hypernets_processor/sequence_processor.py
+++ hypernets_processor/sequence_processor.py
@@ -69,21 +69,25 @@ class SequenceProcessor:
             if self.context.get_config_value("max_level") in ["L1A","L1B","L1C","L2A"]:
                 self.context.logger.info("Processing to L1a...")
                 if l0_rad:
-                    L1a_rad = cal.calibrate_l1a_water("radiance",l0_rad,l0_bla,calibration_data_rad)
+                    L1a_rad, l0_rad_masked, l0_rad_bla_masked = cal.calibrate_l1a(
+                        "radiance", l0_rad, l0_bla, calibration_data_rad
+                    )
                 if l0_irr:
-                    L1a_irr = cal.calibrate_l1a_water("irradiance",l0_irr,l0_bla,calibration_data_irr)
+                    L1a_irr, l0_irr_masked, l0_irr_bla_masked = cal.calibrate_l1a(
+                        "irradiance", l0_irr, l0_bla, calibration_data_irr
+                    )
                 self.context.logger.info("Done")
 
             if l0_rad and l0_irr:
                 if self.context.get_config_value("max_level") in ["L1B","L1C","L2A"]:
                     self.context.logger.info("Processing to L1b radiance...")
-                    L1b_rad = avg.average_l1b("radiance", l0_rad,l0_bla,calibration_data_rad)
+                    L1b_rad = avg.average_l1b("radiance", l0_rad_masked, l0_rad_bla_masked, calibration_data_rad)
                     if self.context.get_config_value("write_l1b"):
                         writer.write(L1b_rad, overwrite=True, remove_vars_strings=self.context.get_config_value("remove_vars_strings"))
                     self.context.logger.info("Done")
 
                     self.context.logger.info("Processing to L1b irradiance...")
-                    L1b_irr = avg.average_l1b("irradiance", l0_irr,l0_bla,calibration_data_irr)
+                    L1b_irr = avg.average_l1b("irradiance", l0_irr_masked, l0_irr_bla_masked, calibration_data_irr)
                     if self.context.get_config_value("write_l1b"):
                         writer.write(L1b_irr, overwrite=True, remove_vars_strings=self.context.get_config_value("remove_vars_strings"))
                     self.context.logger.info("Done")
