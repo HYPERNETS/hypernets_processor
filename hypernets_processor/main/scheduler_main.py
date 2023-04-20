a00132d2c69821f4f84d7c0d36b2c416c5c0d804
Pieter De Vis <pieter.de.vis@npl.co.uk>
2023-04-20 15:54:02 +0100

    merge db to different output file correct averaging description

--- hypernets_processor/main/scheduler_main.py
+++ hypernets_processor/main/scheduler_main.py
@@ -43,6 +43,7 @@ def unpack_scheduler_config(scheduler_config):
     scheduler_config_dict = dict()
 
     sections = [sch for sch in scheduler_config.sections()]
+    print(sections)
     sections.remove("Log")
 
     for sch in sections:
