f6887ff6b30a336a9b30f6f63a964a55c857c42b
Pieter De Vis <pieter.de.vis@npl.co.uk>
2023-06-09 09:06:29 +0100

    merge db to different output file correct averaging description

--- hypernets_processor/data_utils/post_processing_quality_checks.py
+++ hypernets_processor/data_utils/post_processing_quality_checks.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     hour_bins=[0,10,12,14,16,24]
     vzas=[0,5,10,20,30,40,50,60]
     vaas=[83,98,113,263,278,293]
-    for site in ["BAES","WWUK","PEAN"]:
+    for site in ["BASP","WWUK","PEAN"]:
         for vza in vzas:
             for vaa in vaas:
                 times,refl,mask=extract_reflectances(site,wavs,vza,vaa)
