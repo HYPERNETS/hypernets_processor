merge db to different output file correct averaging description

hypernets_processor/data_utils/post_processing_quality_checks.py
@@ -28,18 +28,25 @@ archive_path = r"/home/data/insitu/hypernets/archive_qc"
 
 def make_time_series_plot(wavs,times, measurands, mask, hour_bins, tag):
     for i in range(len(wavs)):
+        print(tag,len(times),len(times[np.where(mask==0)[0]]))
         valids=measurands[np.where(mask==0)[0],i]
         valid_times=times[np.where(mask==0)[0]]
         ax = plt.gca()
         for ii in range(len(hour_bins)-1):
             color = next(ax._get_lines.prop_cycler)['color']
             hour_ids=np.where([time_between(dt.time(),hour_bins[ii],hour_bins[ii+1]) for dt in valid_times])[0]
-            plt.plot(valid_times[hour_ids], valids[hour_ids], "o", color=color,
+            mean,std=sigma_clip(valids[hour_ids], tolerance=0.01, median=True, sigma_thresh=2.0)
+            print("%s:00-%s:00" % (hour_bins[ii], hour_bins[ii + 1]),mean,std)
+            plt.axhline(y=mean, color=color, linestyle='-')
+            plt.axhline(y=mean-2*std, color=color, linestyle=':')
+            plt.axhline(y=mean+2*std, color=color, linestyle=':')
+            ids_outliers=np.where((valids[hour_ids]>mean+2*std) or (valids[hour_ids]<mean-2*std))[0]
+            ids_bestdata=np.where((valids[hour_ids]<mean+2*std) and (valids[hour_ids]>mean-2*std))[0]
+            plt.plot(valid_times[hour_ids][ids_outliers], valids[hour_ids][ids_outliers], "o", color=color,
+                     label="%s:00-%s:00" % (hour_bins[ii], hour_bins[ii + 1]), alpha=0.3)
+            plt.plot(valid_times[hour_ids][ids_bestdata], valids[hour_ids][ids_bestdata], "o", color=color,
                      label="%s:00-%s:00" % (hour_bins[ii], hour_bins[ii + 1]))
-            plt.axhline(y=np.mean(valids[hour_ids]), color=color, linestyle='-')
-            plt.axhline(y=np.mean(valids[hour_ids])-np.std(valids[hour_ids]), color=color, linestyle=':')
-            plt.axhline(y=np.mean(valids[hour_ids])+np.std(valids[hour_ids]), color=color, linestyle=':')
-        plt.plot(times[np.where(mask==1)[0]],measurands[np.where(mask==1)[0],i],"go",alpha=0.2,label="masked by processor")
+        plt.plot(times[np.where(mask==1)[0]],measurands[np.where(mask==1)[0],i],"ko",alpha=0.1,label="masked by processor")
         plt.ylim([min(valids),max(valids)])
         plt.legend()
         plt.ylabel("reflectance")
@@ -63,7 +70,6 @@ def extract_reflectances(site, wavs, vza, vaa):
     refl=np.zeros((len(files),len(wavs)))
     mask=np.ones(len(files))
     times=np.empty(len(files),dtype=datetime.datetime)
-    print(len(files))
     for i in range(len(files)):
         ds=read_hypernets_file(files[i],vza=vza, vaa=vaa, filter_flags=False)
         if ds.quality_flag.values == 0:
@@ -71,7 +77,6 @@ def extract_reflectances(site, wavs, vza, vaa):
         ids=[np.argmin(np.abs(ds.wavelength.values-wav)) for wav in wavs]
         refl[i]=ds.reflectance.values[ids,0]
         times[i]=datetime.datetime.fromtimestamp(ds.acquisition_time.values[0])
-        print(times[i], refl[i], mask[i])
     return times, refl, mask
 
 def read_hypernets_file(filepath, vza=None, vaa=None, nearest=True, filter_flags=True):
@@ -155,7 +160,7 @@ if __name__ == "__main__":
     hour_bins=[0,10,12,14,16,24]
     vzas=[0,5,10,20,30,40,50,60]
     vaas=[83,98,113,263,278,293]
-    for site in ["BASP","WWUK","PEAN"]:
+    for site in ["GHNA","BASP","WWUK","PEAN"]:
         for vza in vzas:
             for vaa in vaas:
                 times,refl,mask=extract_reflectances(site,wavs,vza,vaa)