b4cb34a2cbd6b4c490e4f0393a02902ecc137eff
Pieter De Vis <pieter.de.vis@npl.co.uk>
2023-06-14 11:54:25 +0100

    postprocessing to identify best data without outliers

--- hypernets_processor/data_utils/post_processing_quality_checks.py
+++ hypernets_processor/data_utils/post_processing_quality_checks.py
@@ -111,7 +111,7 @@ def extract_reflectances(files, wavs, vza, vaa, site):
             if ds.quality_flag.values == 0 or (site=="WWUK" and ds.quality_flag.values == 32768):
                 mask[i]=0
             else:
-                print(site,times[i],[DatasetUtil.get_set_flags(flag) for flag in ds["quality_flag"]],files[i])
+                print(site,times[i],ds.quality_flag.values,[DatasetUtil.get_set_flags(flag) for flag in ds["quality_flag"]],files[i])
 
 
         else:
