9e329c19df653aaf371d23e9344f3f63a13241b8
cgoyens <cgoyens@naturalsciences.be>
2023-07-19 14:33:57 +0200

    cleaning up

--- build/lib/hypernets_processor/rhymer/rhymer/shared/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .closest_idx import closest_idx
-# from .config_read import config_read
-# from .download_file import download_file
-# from .interp3d import interp3d
-# from .isodate_to_yday import isodate_to_yday
-# from .lutpos import lutpos
-# from .settings_read import settings_read
