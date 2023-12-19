b1c7869520c71cb0da14dc9035b6136476a7e034
clemgoyens <cgoyens@naturalsciences.be>
2023-12-19 11:12:15 +0100

    corrected input and output arguments for interpolation function

--- hypernets_processor/interpolation/measurement_functions/interpolate_time_linear_coscorrected.py
+++ hypernets_processor/interpolation/measurement_functions/interpolate_time_linear_coscorrected.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class InterpolationTimeLinearCoscorrected(MeasurementFunction):
-    def meas_function(self, output_time, times, irradiance, output_sza, input_sza):
+    def meas_function(self, output_time, input_time, irradiance, output_sza, input_sza):
         """
         This function implements the measurement function.
         Each of the arguments can be either a scalar or a vector (1D-array).
@@ -14,21 +14,21 @@ class InterpolationTimeLinearCoscorrected(MeasurementFunction):
         irradiance = irradiance / np.cos(np.pi / 180.0 * input_sza)
 
         simpleint = True
-        if len(times.shape) > 1:
+        if len(input_time.shape) > 1:
             simpleint = False
             out = np.empty((len(irradiance), len(output_time)))
             for i in range(len(output_time)):
-                if output_time[i] >= np.max(times[:, i]):
-                    out[:, i] = irradiance[:, times[:, i] == np.max(times[:, i]), i][
+                if output_time[i] >= np.max(input_time[:, i]):
+                    out[:, i] = irradiance[:, input_time[:, i] == np.max(input_time[:, i]), i][
                         :, 0
                     ]
-                elif output_time[i] <= np.min(times[:, i]):
-                    out[:, i] = irradiance[:, times[:, i] == np.min(times[:, i]), i][
+                elif output_time[i] <= np.min(input_time[:, i]):
+                    out[:, i] = irradiance[:, input_time[:, i] == np.min(input_time[:, i]), i][
                         :, 0
                     ]
                 else:
                     irradiance_intfunc = scipy.interpolate.interp1d(
-                        times[:, i], irradiance[:, :, i]
+                        input_time[:, i], irradiance[:, :, i]
                     )
                     out[:, i] = irradiance_intfunc(output_time[i])
 
@@ -37,27 +37,27 @@ class InterpolationTimeLinearCoscorrected(MeasurementFunction):
                 simpleint = False
                 out = np.empty((len(irradiance), len(output_time)))
                 for i in range(len(output_time)):
-                    if output_time[i] >= np.max(times):
+                    if output_time[i] >= np.max(input_time):
                         out[:, i] = np.mean(
-                            irradiance[:, times == np.max(times)], axis=1
+                            irradiance[:, input_time == np.max(input_time)], axis=1
                         ).squeeze()
-                    elif output_time[i] <= np.min(times):
+                    elif output_time[i] <= np.min(input_time):
                         out[:, i] = np.mean(
-                            irradiance[:, times == np.min(times)], axis=1
+                            irradiance[:, input_time == np.min(input_time)], axis=1
                         ).squeeze()
                     else:
                         irradiance_intfunc = scipy.interpolate.interp1d(
-                            times, irradiance
+                            input_time, irradiance
                         )
                         out[:, i] = irradiance_intfunc(output_time[i])
 
         if simpleint:
-            if output_time >= max(times):
-                out = irradiance[:, times == max(times)]
-            elif output_time <= min(times):
-                out = irradiance[:, times == min(times)]
+            if output_time >= max(input_time):
+                out = irradiance[:, input_time == max(input_time)]
+            elif output_time <= min(input_time):
+                out = irradiance[:, input_time == min(input_time)]
             else:
-                irradiance_intfunc = scipy.interpolate.interp1d(times, irradiance)
+                irradiance_intfunc = scipy.interpolate.interp1d(input_time, irradiance)
                 out = irradiance_intfunc(output_time)
         return out * np.cos(np.pi / 180.0 * output_sza)
 
@@ -70,20 +70,20 @@ class InterpolationTimeLinearCoscorrected(MeasurementFunction):
 
 
 class WaterNetworkInterpolationSkyRadianceLinearCoscorrected(MeasurementFunction):
-    def meas_function(self, output_time, times, radiance, sza, sza_out):
+    def meas_function(self, output_time, input_time, radiance, output_sza, sza_out):
         """
         This function implements the measurement function.
         Each of the arguments can be either a scalar or a vector (1D-array).
         """
-        radiance = radiance / np.cos(np.pi / 180.0 * sza)
+        radiance = radiance / np.cos(np.pi / 180.0 * output_sza)
 
-        skyradiance_intfunc = scipy.interpolate.interp1d(times, radiance)
+        skyradiance_intfunc = scipy.interpolate.interp1d(input_time, radiance)
 
-        return skyradiance_intfunc(output_time) * np.cos(np.pi / 180.0 * sza_out)
+        return skyradiance_intfunc(output_time) * np.cos(np.pi / 180.0 * output_sza)
 
     @staticmethod
     def get_name():
         return "WaterNetworkInterpolationSkyRadianceLinearCoscorrected"
 
     def get_argument_names(self):
-        return ["output_time", "input_time", "radiance"]
+        return ["output_time", "input_time", "radiance", "output_sza", "input_sza"]
