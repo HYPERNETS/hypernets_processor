c7f4a48578694ea085661eb82b1806e65a6ab747
clemgoyens <cgoyens@naturalsciences.be>
2023-12-21 12:20:12 +0100

    add tab in interpolate_skyradiance

--- hypernets_processor/interpolation/interpolate.py
+++ hypernets_processor/interpolation/interpolate.py
@@ -78,7 +78,7 @@ class Interpolate:
             self.context.logger.info("interpolate sky radiance")
         else:
             print("interpolate sky radiance")
-            dataset_l1c_int = self.interpolate_skyradiance(
+        dataset_l1c_int = self.interpolate_skyradiance(
                 dataset_l1c_int, dataset_l1b_downrad
         )
 
