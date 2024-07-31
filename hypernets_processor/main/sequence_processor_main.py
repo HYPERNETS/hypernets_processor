cfcf63a641b4fd9362760fdb13a15c08520aaf64
Kevin Ruddick <kruddick@naturalsciences.be>
2024-07-31 15:04:23 +0200

    Add "m" anomaly if metadata missing + do not keep trying to reprocess if L2A not achieved and no anomaly

--- hypernets_processor/main/sequence_processor_main.py
+++ hypernets_processor/main/sequence_processor_main.py
@@ -55,7 +55,7 @@ def get_target_sequences(context, to_archive):
                     context.set_config_value("sequence_name", path)
                     context.set_config_value("sequence_path", sequence_path)
                     context.logger.error("metadata.txt not found in directory %s, will try processing again later"%(sequence_path))
-                    #context.anomaly_handler.anomaly_db.add_anomaly("m")
+                    context.anomaly_handler.anomaly_db.add_anomaly("m")
 
     # If adding to archive, remove previously processed paths from list by referencing
     # archive db
@@ -68,7 +68,7 @@ def get_target_sequences(context, to_archive):
             for product in context.archive_db["products"].find(
                 site_id=context.get_config_value("site_id")
             )
-            if "L2A" in product["product_level"]
+            #if "L2A" in product["product_level"] removed 2024-07-17 KGR to prevent infinite loop to reprocess when no L2A and no anomaly
         ]
 
         failed_products = [
