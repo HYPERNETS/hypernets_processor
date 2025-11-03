02ba056a5ea350accad3ad6def0bc890f605a7da
Pieter De Vis <pieter.de.vis@npl.co.uk>
2025-11-03 10:22:54 +0000

    don't reprocess "ms" anomalies

--- hypernets_processor/main/sequence_processor_main.py
+++ hypernets_processor/main/sequence_processor_main.py
@@ -96,7 +96,7 @@ def get_target_sequences(context, to_archive):
             for anomaly in context.anomaly_db["anomalies"].find(
                 site_id=context.get_config_value("site_id")
             )
-            if anomaly["anomaly_id"] not in ['m', 'a', 's', 'o', 'nlu', 'nld', 'ned', 'cl', 'ms', 'd', 'wns', 'hos']
+            if anomaly["anomaly_id"] not in ['m', 'a', 's', 'o', 'nlu', 'nld', 'ned', 'cl', 'd', 'wns', 'hos']
             and not context.get_config_value("reprocess_anomalies")
         ]
 
